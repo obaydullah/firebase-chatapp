058b829c99ac6c2acd21279678ca0ecabea432de
obaydullah <obaydullah.2041@gmail.com>
2022-11-11 19:35:34 +0600

    workds done until group request

--- src/components/home/GroupRequest.js
+++ src/components/home/GroupRequest.js
@@ -1,8 +1,140 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import { BiDotsVerticalRounded } from "react-icons/bi";
+import Cropper from "react-cropper";
+//Firebase Import Start
+import { ref, set, push, onValue, update } from "firebase/database";
+import auth from "../../firebaseConfig";
+import { db } from "../../firebaseConfig";
+//Firebase Import End
+import { PulseLoader } from "react-spinners";
 
 export default function GroupRequest() {
+  const [cropImage, setCropImage] = useState("");
+  const [prevImage, setPrevImage] = useState("");
+  const [cropper, setCropper] = useState("");
+
+  const [groupArr, setGroupArr] = useState([]);
+  const [pendingStr, setPendingStr] = useState("");
+  const [joinedStr, setJoinedStr] = useState("");
+
+  const [groupTitle, setGroupTitle] = useState("");
+  const [groupTagline, setGroupTagline] = useState("");
+
+  const [show, setShow] = useState(false);
+  const [uploadLoading, setUploadLoading] = useState(false);
+
+  useEffect(() => {
+    const groupRef = ref(db, "group");
+    onValue(groupRef, (snapshot) => {
+      let groupArray = [];
+
+      snapshot.forEach((user) => {
+        if (auth.currentUser.uid !== user.val().adminid) {
+          groupArray.push({ ...user.val(), groupkey: user.key });
+        }
+      });
+
+      setGroupArr(groupArray);
+    });
+  }, [db, ref, onValue, auth]);
+
+  //Pending Button
+
+  useEffect(() => {
+    const reqRef = ref(db, "groupjoinrequest");
+    onValue(reqRef, (snapshot) => {
+      let pendingString = "";
+
+      snapshot.forEach((user) => {
+        pendingString += user.val().groupkey + user.val().reqid;
+      });
+
+      setPendingStr(pendingString);
+    });
+  }, [db, ref, onValue, auth]);
+
+  //Joined Button
+  useEffect(() => {
+    const memberRef = ref(db, "memberlist");
+    onValue(memberRef, (snapshot) => {
+      let joinedString = "";
+
+      snapshot.forEach((user) => {
+        if (auth.currentUser.uid === user.val().reqid) {
+          joinedString += user.val().groupkey;
+        }
+      });
+
+      setJoinedStr(joinedString);
+    });
+  }, [db, ref, onValue, auth]);
+
+  // Image Crop Start
+  const cropperRef = useRef();
+  const onCrop = () => {
+    const imageElement = cropperRef?.current;
+    const cropper = imageElement?.cropper;
+    // console.log(cropper.getCroppedCanvas().toDataURL());
+    setPrevImage(cropper.getCroppedCanvas().toDataURL());
+  };
+  // Image Crop End
+
+  const handleShow = () => {
+    setShow((prevState) => {
+      return !prevState;
+    });
+  };
+
+  const handleModalClose = (e) => {
+    if (e.target.className.includes("close-div")) {
+      setShow(false);
+      setPrevImage("");
+      setCropImage("");
+    }
+  };
+
+  const handleSelectImage = (e) => {
+    let files = e.target.files[0];
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      setCropImage(event.target.result);
+    };
+    reader.readAsDataURL(files);
+  };
+
+  const handleCreateGroup = (e) => {
+    e.preventDefault();
+    setUploadLoading(true);
+
+    if (typeof cropper !== "undefined") {
+      let imageURL = cropper.getCroppedCanvas().toDataURL();
+      set(push(ref(db, "group")), {
+        groupTitle,
+        groupTagline,
+        adminid: auth.currentUser.uid,
+        adminname: auth.currentUser.displayName,
+        groupimage: imageURL,
+      }).then(() => {
+        setUploadLoading(false);
+        setShow(false);
+        setPrevImage("");
+        setCropImage("");
+      });
+    }
+  };
+
+  const handleJoin = (groupitem) => {
+    set(push(ref(db, "groupjoinrequest")), {
+      ...groupitem,
+      reqid: auth.currentUser.uid,
+      reqphoto: auth.currentUser.photoURL,
+      reqname: auth.currentUser.displayName,
+    });
+  };
+
   return (
     <>
       <div className="h-[365px] overflow-hidden">
@@ -24,92 +156,140 @@ export default function GroupRequest() {
         <div className="shadow-xl p-4 h-full overflow-y-scroll">
           <div className="flex justify-between mb-2">
             <h2 className="text-black font-bold text-xl ">Group Request</h2>
+            <button
+              className="bg-green-600 text-white p-2 rounded"
+              onClick={handleShow}
+            >
+              Create group
+            </button>
             <BiDotsVerticalRounded className="text-green-600 text-2xl cursor-pointer" />
           </div>
 
           {/* Single Group Request Start */}
-          <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-            <img
-              src="./images/profile.png"
-              alt=""
-              className="h-[60px] w-[60px]"
-            />
-            <div className="sm:mr-0 sml:mr-10">
-              <h2 className="font-bold sm:text-base sml:text-xl ">
-                Friends Reunioin
-              </h2>
-              <p className="text-sm text-gray-500">Hi guys, Whats up</p>
+
+          {groupArr.map((group, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center border-b border-solid border-gray-300 py-2"
+            >
+              <img
+                src={group.groupimage}
+                alt=""
+                className="h-[60px] w-[60px] rounded-full"
+              />
+              <div className="sm:mr-0 sml:mr-auto sml:ml-3">
+                <h2 className="font-bold sm:text-base sml:text-xl ">
+                  {group.groupTitle}
+                </h2>
+                <p className="text-sm text-gray-500">{group.groupTagline}</p>
+                <p className="text-sm text-gray-900">
+                  Admin : {group.adminname}
+                </p>
+              </div>
+
+              {pendingStr.includes(group.groupkey) &&
+              pendingStr.includes(auth.currentUser.uid) ? (
+                <button className="bg-red-600 text-white px-4 py-2 rounded text-base">
+                  Pending
+                </button>
+              ) : joinedStr.includes(group.groupkey) ? (
+                <button className="bg-green-600 text-white px-4 py-2 rounded text-base">
+                  Joined
+                </button>
+              ) : (
+                <button
+                  className="bg-green-600 text-white px-4 py-2 rounded text-base"
+                  onClick={() => handleJoin(group)}
+                >
+                  Join
+                </button>
+              )}
             </div>
-            <button className="bg-green-600 text-white px-4 py-2 rounded text-base">
-              Accept
-            </button>
-          </div>
-          <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-            <img
-              src="./images/profile.png"
-              alt=""
-              className="h-[60px] w-[60px]"
-            />
-            <div className="sm:mr-0 sml:mr-10">
-              <h2 className="font-bold sm:text-base sml:text-xl ">
-                Friends Reunioin
-              </h2>
-              <p className="text-sm text-gray-500">Hi guys, Whats up</p>
-            </div>
-            <button className="bg-green-600 text-white px-4 py-2 rounded text-base">
-              Accept
-            </button>
-          </div>
-          <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-            <img
-              src="./images/profile.png"
-              alt=""
-              className="h-[60px] w-[60px]"
-            />
-            <div className="sm:mr-0 sml:mr-10">
-              <h2 className="font-bold sm:text-base sml:text-xl ">
-                Friends Reunioin
-              </h2>
-              <p className="text-sm text-gray-500">Hi guys, Whats up</p>
-            </div>
-            <button className="bg-green-600 text-white px-4 py-2 rounded text-base">
-              Accept
-            </button>
-          </div>
-          <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-            <img
-              src="./images/profile.png"
-              alt=""
-              className="h-[60px] w-[60px]"
-            />
-            <div className="sm:mr-0 sml:mr-10">
-              <h2 className="font-bold sm:text-base sml:text-xl ">
-                Friends Reunioin
-              </h2>
-              <p className="text-sm text-gray-500">Hi guys, Whats up</p>
-            </div>
-            <button className="bg-green-600 text-white px-4 py-2 rounded text-base">
-              Accept
-            </button>
-          </div>
-          <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-            <img
-              src="./images/profile.png"
-              alt=""
-              className="h-[60px] w-[60px]"
-            />
-            <div className="sm:mr-0 sml:mr-10">
-              <h2 className="font-bold sm:text-base sml:text-xl ">
-                Friends Reunioin
-              </h2>
-              <p className="text-sm text-gray-500">Hi guys, Whats up</p>
-            </div>
-            <button className="bg-green-600 text-white px-4 py-2 rounded text-base">
-              Accept
-            </button>
-          </div>
+          ))}
         </div>
       </div>
+
+      {/* Image Upload Modal Start  */}
+
+      {show && (
+        <div
+          className="w-full h-screen bg-green-600/75 text-black flex justify-center items-center fixed top-0 left-0 close-div z-20"
+          onClick={handleModalClose}
+        >
+          <div className="bg-white rounded p-10 relative h-[500px] w-[500px]">
+            <div className="absolute top-4 right-4 text-xl cursor-pointer p-2 w-12 close-div">
+              <img src="./images/close.jpg" alt="" className="close-div" />
+            </div>
+            <form>
+              <h2 className="text-green-600 text-xl font-bold mb-3">
+                Upload Image
+              </h2>
+
+              <img
+                src={prevImage || auth.currentUser.photoURL}
+                alt=""
+                className="h-[70px] w-[70px] mr-3 rounded-full"
+              />
+              <input
+                type="file"
+                name="file"
+                id="file"
+                className=" rounded py-4 focus:outline-none w-[300px]"
+                onChange={handleSelectImage}
+              />
+              <input
+                type="text"
+                name="grouptitle"
+                id="grouptitle"
+                className="border border-solid border-gray-500 rounded px-7 py-2 focus:outline-none w-full mb-4"
+                placeholder="Group Title"
+                value={groupTitle}
+                onChange={(e) => setGroupTitle(e.target.value)}
+              />
+              <input
+                type="text"
+                name="grouptitle"
+                id="grouptitle"
+                className="border border-solid border-gray-500 rounded px-7 py-2 focus:outline-none w-full"
+                placeholder="Group Tagline"
+                value={groupTagline}
+                onChange={(e) => setGroupTagline(e.target.value)}
+              />
+
+              {uploadLoading ? (
+                <PulseLoader
+                  color="green"
+                  className="text-center text-green-600"
+                />
+              ) : (
+                <button
+                  type="submit"
+                  className="bg-green-700 text-white block rounded-full text-lg w-full p-4 mt-4 mb-2"
+                  onClick={handleCreateGroup}
+                >
+                  Create group
+                </button>
+              )}
+
+              {/* Crop Component  */}
+              <Cropper
+                src={cropImage}
+                // style={{ height: 400, width: "100%" }}
+                className="w-[400px] !h-[250px]"
+                // Cropper.js options
+                initialAspectRatio={8 / 5}
+                guides={false}
+                crop={onCrop}
+                ref={cropperRef}
+                onInitialized={(instance) => {
+                  setCropper(instance);
+                }}
+              />
+            </form>
+          </div>
+        </div>
+      )}
+      {/* Image Upload Modal End  */}
     </>
   );
 }
--- src/components/home/MyGroup.js
+++ src/components/home/MyGroup.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BiDotsVerticalRounded } from "react-icons/bi";
+import { ref, onValue, remove, set, push, update } from "firebase/database";
+import { db } from "../../firebaseConfig";
+import auth from "../../firebaseConfig";
 
 export default function MyGroup() {
+  const [myGroup, setMyGroup] = useState([]);
+  const [groupRequest, setGroupRequest] = useState([]);
+  const [membersArr, setMembersArr] = useState([]);
+
+  const [infoShow, setInfoShow] = useState(false);
+  const [memberShow, setMemberShow] = useState(false);
+
+  useEffect(() => {
+    const groupRef = ref(db, "group");
+    onValue(groupRef, (snapshot) => {
+      let groupArray = [];
+
+      snapshot.forEach((user) => {
+        if (auth.currentUser.uid == user.val().adminid) {
+          groupArray.push({ ...user.val(), groupid: user.key });
+        }
+      });
+
+      setMyGroup(groupArray);
+    });
+  }, []);
+
+  const handleReqShow = (group) => {
+    setInfoShow((prevState) => {
+      return !prevState;
+    });
+
+    const groupjoinRef = ref(db, "groupjoinrequest");
+    onValue(groupjoinRef, (snapshot) => {
+      let groupJoinArray = [];
+
+      snapshot.forEach((user) => {
+        if (group.groupid === user.val().groupkey) {
+          groupJoinArray.push({ ...user.val(), key: user.key });
+        }
+      });
+
+      setGroupRequest(groupJoinArray);
+    });
+  };
+
+  const handleMember = (member) => {
+    setMemberShow(!memberShow);
+
+    const memberRef = ref(db, "memberlist");
+    onValue(memberRef, (snapshot) => {
+      let memberArray = [];
+
+      snapshot.forEach((user) => {
+        if (member.groupid === user.val().groupkey) {
+          memberArray.push(user.val());
+        }
+      });
+
+      setMembersArr(memberArray);
+    });
+  };
+
+  const handleRejectRequest = (group) => {
+    remove(ref(db, "groupjoinrequest/" + group.key));
+  };
+
+  const handleAcceptReq = (group) => {
+    set(push(ref(db, "memberlist/")), {
+      adminid: group.adminid,
+      adminname: group.adminname,
+      groupTagline: group.groupTagline,
+      groupTitle: group.groupTitle,
+      groupimage: group.groupimage,
+      groupkey: group.groupkey,
+      reqid: group.reqid,
+      reqname: group.reqname,
+      reqphoto: group.reqphoto,
+    })
+      .then(() => {
+        remove(ref(db, "groupjoinrequest/" + group.key));
+      })
+      .then(() => {
+        setInfoShow(false);
+      });
+  };
+
   return (
     <>
       {/* Friends Start */}
@@ -12,68 +97,106 @@ export default function MyGroup() {
         </div>
 
         {/* Single Friend Start */}
-        <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-          <img
-            src="./images/profile.png"
-            alt=""
-            className="h-[50px] w-[50px]"
-          />
-          <div>
-            <h2 className="text-base font-bold">Friends Reunion</h2>
-            <p className="text-sm text-gray-500">Hi guys, Whats up</p>
-          </div>
-          <p className="text-[10px] text-gray-400">Today: 9.00pm</p>
-        </div>
 
-        <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-          <img
-            src="./images/profile.png"
-            alt=""
-            className="h-[50px] w-[50px]"
-          />
-          <div>
-            <h2 className="text-base font-bold">Friends Reunion</h2>
-            <p className="text-sm text-gray-500">Hi guys, Whats up</p>
-          </div>
-          <p className="text-[10px] text-gray-400">Today: 9.00pm</p>
-        </div>
-
-        <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-          <img
-            src="./images/profile.png"
-            alt=""
-            className="h-[50px] w-[50px]"
-          />
-          <div>
-            <h2 className="text-base font-bold">Friends Reunion</h2>
-            <p className="text-sm text-gray-500">Hi guys, Whats up</p>
-          </div>
-          <p className="text-[10px] text-gray-400">Today: 9.00pm</p>
-        </div>
-        <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-          <img
-            src="./images/profile.png"
-            alt=""
-            className="h-[50px] w-[50px]"
-          />
-          <div>
-            <h2 className="text-base font-bold">Friends Reunion</h2>
-            <p className="text-sm text-gray-500">Hi guys, Whats up</p>
-          </div>
-          <p className="text-[10px] text-gray-400">Today: 9.00pm</p>
-        </div>
-        <div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
-          <img
-            src="./images/profile.png"
-            alt=""
-            className="h-[50px] w-[50px]"
-          />
-          <div>
-            <h2 className="text-base font-bold">Friends Reunion</h2>
-            <p className="text-sm text-gray-500">Hi guys, Whats up</p>
-          </div>
-          <p className="text-[10px] text-gray-400">Today: 9.00pm</p>
-        </div>
+        {infoShow ? (
+          <>
+            <div className="flex justify-between">
+              <h2 className="text-green-600 text-lg font-bold">Req List</h2>
+              <button
+                className="bg-green-600 text-white p-2 rounded"
+                onClick={() => setInfoShow((prevState) => !prevState)}
+              >
+                Back
+              </button>
+            </div>
+            {groupRequest.map((group, index) => (
+              <div
+                key={index}
+                className="flex justify-between items-center border-b border-solid border-gray-300 py-2"
+              >
+                <img
+                  src={group.reqphoto}
+                  alt=""
+                  className="h-[50px] w-[50px] rounded-full"
+                />
+                <div className="sm:mr-0 sml:mr-auto sml:ml-3 mr-2">
+                  <h2 className="text-base font-bold"> {group.reqname}</h2>
+                  <p className="text-sm text-gray-500">Whats up!</p>
+                </div>
+                <button
+                  className="bg-green-600 text-white p-2 rounded mr-2"
+                  onClick={() => handleAcceptReq(group)}
+                >
+                  Accept
+                </button>
+                <button
+                  className="bg-red-600 text-white p-2 rounded"
+                  onClick={() => handleRejectRequest(group)}
+                >
+                  Reject
+                </button>
+              </div>
+            ))}
+          </>
+        ) : memberShow ? (
+          <>
+            <div className="flex justify-between">
+              <h2 className="text-green-600 text-lg font-bold">Member List</h2>
+              <h3>Total {membersArr.length} </h3>
+              <button
+                className="bg-green-600 text-white p-2 rounded"
+                onClick={() => setMemberShow((prevState) => !prevState)}
+              >
+                Back
+              </button>
+            </div>
+            {membersArr.map((member, index) => (
+              <div
+                key={index}
+                className="flex justify-between items-center border-b border-solid border-gray-300 py-2"
+              >
+                <img
+                  src={member.reqphoto}
+                  alt=""
+                  className="h-[50px] w-[50px] rounded-full"
+                />
+                <div className="sm:mr-0 sml:mr-auto sml:ml-3 mr-2">
+                  <h2 className="text-base font-bold"> {member.reqname}</h2>
+                  <p className="text-sm text-gray-500">Whats up!</p>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          myGroup.map((group, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center border-b border-solid border-gray-300 py-2"
+            >
+              <img
+                src={group.groupimage}
+                alt=""
+                className="h-[50px] w-[50px] rounded-full"
+              />
+              <div className="sm:mr-0 sml:mr-auto sml:ml-3 mr-2">
+                <h2 className="text-base font-bold"> {group.groupTitle}</h2>
+                <p className="text-sm text-gray-500">{group.groupTagline}</p>
+              </div>
+              <button
+                className="bg-green-600 text-white p-2 rounded mr-2"
+                onClick={() => handleReqShow(group)}
+              >
+                Info
+              </button>
+              <button
+                className="bg-green-600 text-white p-2 rounded"
+                onClick={() => handleMember(group)}
+              >
+                Member
+              </button>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
